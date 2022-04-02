1a204014bb2e21b1e8b90a2df3739901f50b38cf
Olga <olgaarakelyan515@gmail.com>
2022-04-02 22:36:15 +0400

    first-commit

---
+++ recipes/rec.js
@@ -0,0 +1,66 @@
+let karkandak1 = document.getElementById('karkandak1');
+let karkandak2 = document.getElementById('karkandak2');
+let karkandak3 = document.getElementById('karkandak3');
+let aghcan1 = document.getElementById('aghcan1');
+let aghcan2 = document.getElementById('aghcan2');
+let aghcan3 = document.getElementById('aghcan3');
+let tort1 = document.getElementById('tort1');
+let tort2 = document.getElementById('tort2');
+let tort3 = document.getElementById('tort3');
+let div1 = document.getElementById('div1');
+let div2 = document.getElementById('div2');
+let div3 = document.getElementById('div3');
+let div4 = document.getElementById('div4');
+let div5 = document.getElementById('div5');
+let div6 = document.getElementById('div6');
+let div7 = document.getElementById('div7');
+let div8 = document.getElementById('div8');
+let div9 = document.getElementById('div9');
+let sec_div = document.getElementById('sec_div');
+
+karkandak1.addEventListener('click', () => {
+    sec_div.style.display = 'none'
+    div1.style.display = 'block'
+})
+
+karkandak2.addEventListener('click', () => {
+    sec_div.style.display = 'none'
+    div2.style.display = 'block'
+})
+
+karkandak3.addEventListener('click', () => {
+    sec_div.style.display = 'none'
+    div3.style.display = 'block'
+})
+
+aghcan1.addEventListener('click', () => {
+    sec_div.style.display = 'none'
+    div4.style.display = 'block'
+})
+
+aghcan2.addEventListener('click',()=>{
+    sec_div.style.display = 'none'
+    div5.style.display = 'block'
+})
+
+aghcan3.addEventListener('click',()=>{
+    sec_div.style.display = 'none'
+    div6.style.display = 'block'
+})
+
+tort1.addEventListener('click',()=>{
+    sec_div.style.display = 'none'
+    div7.style.display = 'block'
+})
+
+
+tort2.addEventListener('click',()=>{
+    sec_div.style.display = 'none'
+    div8.style.display = 'block'
+})
+
+
+tort3.addEventListener('click',()=>{
+    sec_div.style.display = 'none'
+    div9.style.display = 'block'
+})
